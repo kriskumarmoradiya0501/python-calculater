695d35d8e7abd2c848f71ca464c72557c557d4d8
kriskumarmoradiya0501 <140172827+kriskumarmoradiya0501@users.noreply.github.com>
2023-07-21 18:15:04 +0530

    Add files via upload

---
+++ py_7_cal.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     20/07/2023
+# Copyright:   (c) Administrator 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+a = 50
+#variable declaration
+b = 12
+#variable declaration
+
+print(" A + B = ",a+b)
+#perform addition
+print(" A / B = ",a/b)
+#perform dividetion
+print(" A * B = ",a*b)
+#perform multiplication
+print(" A - B = ",a-b)
+# perform subtraction
+print(" A % B = ",a%b)
+#find modulo
